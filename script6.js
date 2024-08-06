393318afe395fba8ae2332e53201f960cd480552
lucas <lucastucha6@gmail.com>
2024-08-06 00:00:13 -0300

    meu terceiro commit

--- script6.js
+++ script6.js
@@ -15,23 +15,23 @@ for(n=0;n<=platina;n+=1000)
         
 }
 if(n===ferro){
-    console.log("vc e ferro")
+    console.log("nome da sua heroina e",nome,"vc e ferro")
 
 }
 else if(n===bronze){
-    console.log("vc e bronze")
+    console.log("nome da sua heroina e",nome,"vc e bronze")
 }
 else if(n===prata){
-    console.log("vc e prata")
+    console.log("nome da sua heroina e",nome,"vc e prata")
 }
 else if(n===ouro){
-    console.log("vc e ouro")
+    console.log("nome da sua heroina e",nome,"vc e ouro")
 }
 else if(n===platina){
-    console.log("vc e platina")
+    console.log("nome da sua heroina e",nome,"vc e platina")
 }
 else if(n===ascendente){
     console.log("nome da sua heroina e",nome, ",e vc esta no nivel ascendente")
 }
-nivel
+
 
