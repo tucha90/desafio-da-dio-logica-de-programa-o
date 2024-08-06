35d6aa0a865da11f19734e0f6aba71ff4c6ecd18
lucas <lucastucha6@gmail.com>
2024-08-05 23:24:05 -0300

    meu primeiro commit

---
+++ script6.js
@@ -0,0 +1,37 @@
+let nome="rubi"
+let nivel=
+
+ferro=0
+bronze=1000
+prata=2000
+ouro=3000 
+platina=4000
+ascendente=5000
+imortal=6000
+radiante=7000
+
+for(n=0;n<=platina;n+=1000)
+    {console.log(n)
+        
+}
+if(n===ferro){
+    console.log("vc e ferro")
+
+}
+else if(n===bronze){
+    console.log("vc e bronze")
+}
+else if(n===prata){
+    console.log("vc e prata")
+}
+else if(n===ouro){
+    console.log("vc e ouro")
+}
+else if(n===platina){
+    console.log("vc e platina")
+}
+else if(n===ascendente){
+    console.log("nome da sua heroina e",nome, ",e vc esta no nivel ascendente")
+}
+nivel
+
